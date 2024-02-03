d54d6ef24a59ad92bad1f1d102c7dacb01d6eef5
monirul4213 <monirul4213@gmail.com>
2024-02-03 21:02:59 +0600

    project created

--- src/Component/Skillsection/SkillSection.jsx
+++ src/Component/Skillsection/SkillSection.jsx
@@ -1,53 +1,152 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SkillSection.css";
 import Frontend from "./../../Assets/Icon/frontend.png";
 import Backend from "./../../Assets/Icon/backend.png";
 import Tools from "./../../Assets/Icon/tools.png";
 import SoftSkill from "./../../Assets/Icon/softskill.png";
 
-
 const SkillSection = () => {
-  
-  
-    
+  const [title, setTitle] = useState("Tools");
+  const [data, SetData] = useState([
+    {
+      title: "HTML 5",
+      value: 95,
+    },
+    {
+      title: "CSS 3",
+      value: 90,
+    },
+    {
+      title:'JavaScript',
+      value: 85
+    },
+    {
+      title:'React.Js',
+      value: 90
+    },
+    {
+      title:'Flutter',
+      value: 90
+    }
+  ]);
+
+  const skillChange = (e, obj) => {
+    setTitle(e.target.textContent);
+    SetData(obj);
+  };
+
   return (
     <div className="SkillSection">
       <h1>Technical Proficiency</h1>
       <div className="skillBar">
-        <div class="gridSection">
-          <div className="grid">
+        <div className="gridSection">
+          <div
+            className="grid"
+            onClick={(e) =>
+              skillChange(e, [
+                {
+                  title: "HTML 5",
+                  value: 95,
+                },
+                {
+                  title: "CSS 3",
+                  value: 90,
+                },
+                {
+                  title:'JavaScript',
+                  value: 85
+                },
+                {
+                  title:'React.Js',
+                  value: 90
+                },
+                {
+                  title:'Flutter',
+                  value: 90
+                }
+              ])
+            }
+          >
             <p>Frontend</p>
             <img src={Frontend} />
           </div>
-          <div className="grid">
+          <div
+            className="grid"
+            onClick={(e) =>
+              skillChange(e, [
+                {
+                  title: "Node.Js",
+                  value: 80,
+                },
+                {
+                  title: "Express.Js",
+                  value: 90,
+                },
+              ])
+            }
+          >
             <p>Backend</p>
             <img src={Backend} />
           </div>
-          <div className="grid">
+          <div className="grid" onClick={(e)=>skillChange(e,[
+            {
+              title:'Git & GitHub',
+              value: 75
+             },
+             {
+               title:'Visual Studio Code',
+               value: 90
+              },
+              {
+               title:'Responsive Design',
+               value: 80
+              },
+              {
+               title:'Framework',
+               value: 85
+              },
+          ])}>
             <p>Tools</p>
             <img src={Tools} />
           </div>
-          <div className="grid">
+          <div className="grid" onClick={(e)=>skillChange(e,[
+             {
+              title: "Problem Solving",
+              value: 80,
+            },
+            {
+              title: "Collaboration",
+              value: 90,
+            },
+            {
+              title: "Attention to Details",
+              value: 85,
+            }
+          ])}>
             <p>Soft Skill</p>
             <img src={SoftSkill} />
           </div>
         </div>
-        <div className="skillsBar"></div>
+
         <div className="tableContainer">
-         <div className="MainTable">
-           <h3>Tools</h3>
-           <hr></hr>
-         <div className="progressBar">
-          <h4>Git & GitHub</h4>
-         <progress value={75} max={100} className="CustomProgressbar" />
-         <h4>Visual Studio Code</h4>
-         <progress value={90} max={100} className="CustomProgressbar" />
-         <h4>Responsive Desine</h4>
-         <progress value={80} max={100} className="CustomProgressbar" />
-         <h4>Framework</h4>
-         <progress value={86} max={100} className="CustomProgressbar" />
-         </div>
-         </div>
+          <div className="MainTable">
+            <h3>{title}</h3>
+            <hr></hr>
+            <div className="progressBar">
+              {data.map((skill, idx) => {
+                return (
+                  <div key={idx}>
+                    <h4>{skill.title}</h4>
+                    <progress
+                      value={skill.value}
+                      max="100"
+                      className="CustomProgressbar"
+                    />
+                  </div>
+                );
+              })}
+            </div>
+          </div>
         </div>
       </div>
     </div>
